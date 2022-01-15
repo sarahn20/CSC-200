e7dc183377a68fc27e7bfb3608d2fa0a99285673
sarahn20 <56127151+sarahn20@users.noreply.github.com>
2022-01-15 12:08:44 -0500

    First commit - Program #1 - 2/19/19

---
+++ SNPython1.py
@@ -0,0 +1,21 @@
+#Assignment Description: Get the code we wrote in class that is linked on slack working using your text editor and running from the command line.
+#This code essentially a copy of what we learned in class
+
+# Variables
+fname = "Sarah"
+lname = "N"
+age = 18
+
+print(fname)
+print(lname)
+print(age)
+print(fname + " " + lname)
+if(age >= 21):
+  print("Let's hit the bar")
+else:
+  print("Let's hit the arcade")
+for i in range(1, 11):
+  print(i)
+while age >= 0:
+  print(age)
+  age = age - 1
